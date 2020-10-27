c6ab6a521d50a6cda750dd1b3bcb9245cec73702
Suho Kim <skim2@fnal.gov>
2020-10-27 13:08:28 -0500

    edit hadd_TF

--- TransferFactor/hadd_TF.py
+++ TransferFactor/hadd_TF.py
@@ -14,12 +14,12 @@ from ROOT import *
 
 
 #Region = ['TwoEleDY','TwoMuDY']
-Samples = ['DYJetsToLL_M-50']
+Samples = ['DY']
 #Samples = ['ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-1','ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-10','ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-100','ZH_HToSSTobbbb_ZToLL_MH-125_MS-15_ctauS-1000','ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-1','ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-10','ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-100','ZH_HToSSTobbbb_ZToLL_MH-125_MS-40_ctauS-1000','ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-1','ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-10','ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-100','ZH_HToSSTobbbb_ZToLL_MH-125_MS-55_ctauS-1000']
 Region = ['hadd']
 TV = ['nSelectedAODCaloJetTag']
 #Unc = ['MES','EGS','LeptonSF','AMax','IPSig','TA','TagVars']
-Unc = ['MSF']
+Unc = ['JES']
 gROOT.ForceStyle(kTRUE)
 	
 #gStyle.SetOptStat(0)
@@ -43,14 +43,18 @@ for m in range(len(Samples)):
 				Var = TV[j]
 				unc = Unc[k]
 				file_object=open("TF_%s_%s_%s_%s.txt" %(Channel,Var,unc,sample),"a")
-				_file0 = TFile.Open('/uscms/home/skim2/nobackup/2018-LLDJ_slc7_700_CMSSW_10_2_5/src/2018lldj/plots/Separate_LeptonSF/%sZH/%sZH_%s_log_%sUp_%s.root' %(Channel,Channel,Var,unc,sample),'read')
-				_file1 = TFile.Open('/uscms/home/skim2/nobackup/2018-LLDJ_slc7_700_CMSSW_10_2_5/src/2018lldj/plots/Separate_LeptonSF/%sDY/%sDY_%s_log_%sUp_%s.root' %(Channel,Channel,Var,unc,sample),'read')
-				h0=_file0.Get('Sig_NoUnc')
-				h1=_file1.Get('Sig_NoUnc')
-				h2=_file0.Get('Sig_UncUp')
-				h3=_file1.Get('Sig_UncUp')
-				h4=_file0.Get('Sig_UncDown')
-				h5=_file1.Get('Sig_UncDown')	
+				_file3 = TFile.Open('/uscms/home/skim2/nobackup/LLDJ_slc7_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/Limitsetup_JECSys/%sDY/%sDY_%s_GH_%sUp_wSF.root' %(Channel,Channel,Var,unc),'read')
+				_file2 = TFile.Open('/uscms/home/skim2/nobackup/LLDJ_slc7_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/Limitsetup_JECSys/%sZH/%sZH_%s_GH_%sUp_wSF.root' %(Channel,Channel,Var,unc),'read')
+				_file5 = TFile.Open('/uscms/home/skim2/nobackup/LLDJ_slc7_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/Limitsetup_JECSys/%sDY/%sDY_%s_GH_%sDown_wSF.root' %(Channel,Channel,Var,unc),'read')
+				_file4 = TFile.Open('/uscms/home/skim2/nobackup/LLDJ_slc7_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/Limitsetup_JECSys/%sZH/%sZH_%s_GH_%sDown_wSF.root' %(Channel,Channel,Var,unc),'read')
+				_file1 = TFile.Open('/uscms/home/skim2/nobackup/LLDJ_slc7_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/Limitsetup_JECSys/%sDY/%sDY_%s_GH_wSF.root' %(Channel,Channel,Var),'read')
+				_file0 = TFile.Open('/uscms/home/skim2/nobackup/LLDJ_slc7_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/Limitsetup_JECSys/%sZH/%sZH_%s_GH_wSF.root' %(Channel,Channel,Var),'read')
+				h0=_file0.Get('DY')
+				h1=_file1.Get('DY')
+				h2=_file2.Get('DY')
+				h3=_file3.Get('DY')
+				h4=_file4.Get('DY')
+				h5=_file5.Get('DY')
 
 				h0.Divide(h1)
 				h0.SetTitle('%s_%s_%s Transfer Factor (ZH/DY)' %(Channel,Var,unc))
@@ -73,7 +77,6 @@ for m in range(len(Samples)):
 				h2.SetLineStyle(9)
 				h2.Draw('HIST same')
 				h2.GetXaxis().SetRangeUser(0,2)
-				h2.GetYaxis().SetRangeUser(0,0)
 		
 				h7 = TH1F()
 				h7 = h4.Clone()
@@ -88,7 +91,6 @@ for m in range(len(Samples)):
 				h6.SetLineStyle(2)
 				h6.Draw('HIST same')
 				h6.GetXaxis().SetRangeUser(0,2)
-				h6.GetYaxis().SetRangeUser(0,0)
 				print(h2.GetBinContent(3))			
 				print(h6.GetBinContent(3))			
 
@@ -110,16 +112,24 @@ for m in range(len(Samples)):
 				file_object.seek(0)
 				file_object.write(sample)
 				file_object.write("\n")
-				file_object.write("      NoUnc            Up/Up      %diff       Down/Down       %diff       Up/Down       %diff       Down/Up       %diff   ")
-				file_object.write("\n")
+				#file_object.write("      NoUnc            Up/Up      %diff       Down/Down       %diff       Up/Down       %diff       Down/Up       %diff   ")
 				
 				for t in range(3):
 					if t == 0 :
 						file_object.write("0Tag bin ")
+						file_object.write("\n")
+						file_object.write("      NoUnc          ")
+						file_object.write("\n")
 					if t == 1 :
 						file_object.write("1Tag bin ")
+						file_object.write("\n")
+						file_object.write("      NoUnc          ")
+						file_object.write("\n")
 					if t == 2 :
 						file_object.write("2Tag bin ")
+						file_object.write("\n")
+						file_object.write("      NoUnc          ")
+						file_object.write("\n")
 					f0 =round(h0.GetBinContent(t+1),7)
 					f2 =round(h2.GetBinContent(t+1),7)
 					f4 =round(h4.GetBinContent(t+1),7)
@@ -132,13 +142,25 @@ for m in range(len(Samples)):
 					#file_object.write(str(round(h6.GetBinContent(i+1),7))+'   ' )
 					#file_object.write(str(round(h7.GetBinContent(i+1),7))+'   ' )
 					file_object.write(str(f0)+'       ' )
+					file_object.write("\n")
 					if f0 > 0 :
+						file_object.write("      Up/Up    %diff      ")
+						file_object.write("\n")
 						file_object.write(str(f2)+'   ' )
 						file_object.write(str(round((f2-f0)/f0,7))+'   ' )
+						file_object.write("\n")
+						file_object.write("      Down/Down    %diff      ")
+						file_object.write("\n")
 						file_object.write(str(f4)+'   ' )
 						file_object.write(str(round((f4-f0)/f0,7))+'   ' )
+						file_object.write("\n")
+						file_object.write("      Up/Down    %diff      ")
+						file_object.write("\n")
 						file_object.write(str(f6)+'   ' )
 						file_object.write(str(round((f6-f0)/f0,7))+'   ' )
+						file_object.write("\n")
+						file_object.write("      Down/Up    %diff      ")
+						file_object.write("\n")
 						file_object.write(str(f7)+'   ' )
 						file_object.write(str(round((f7-f0)/f0,7))+'   ' )
 						file_object.write("\n")
@@ -182,7 +204,7 @@ for m in range(len(Samples)):
 				extra2.SetTextFont(62)
 	    			title.DrawTextNDC(0.2,0.91,"CMS")
 	    			extra.DrawTextNDC(0.3,0.91,"Preliminary")
-	    			extra2.DrawLatexNDC(0.55,0.91,"Run2 2018 58.67/fb")
+	    			extra2.DrawLatexNDC(0.55,0.91,"Run2 ErasGH 16.1/fb")
 				c1.Modified()
 				c1.Update()
 				k=raw_input('Press ENTER to exit')
